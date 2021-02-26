86c003bdcbceb94d096405cc4101d8d93ca06355
sweisz <dev@steffenweisz.de>
2021-02-26 10:33:27 +0100

    Added delete option on tracks

--- components/TrackDetails.tsx
+++ components/TrackDetails.tsx
@@ -1,17 +1,22 @@
+import { Router } from "next/router";
 import styles from "../styles/TrackDetails.module.css";
+import { deleteTrack } from "../utils/api";
 
 type Props = {
+  id: string;
   imgSrc: string;
   title: string;
   artist: string;
 };
 
-export default function TrackDetails({ imgSrc, title, artist }: Props) {
+export default function TrackDetails({ id, imgSrc, title, artist }: Props) {
   return (
-    <div className={styles.content}>
-      <img className={styles.img} src={imgSrc} alt="" />
-      <div className={styles.title}> {title}</div>
-      <div className={styles.artist}> {artist}</div>
-    </div>
+    <>
+      <div className={styles.content}>
+        <img className={styles.img} src={imgSrc} alt="" />
+        <div className={styles.title}> {title}</div>
+        <div className={styles.artist}> {artist}</div>
+      </div>
+    </>
   );
 }
--- pages/tracks/[id].tsx
+++ pages/tracks/[id].tsx
@@ -1,10 +1,10 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { APITrack, getTrack } from "../../utils/api";
 import styles from "../../styles/TrackItem.module.css";
 import AudioPlayer from "../../components/AudioPlayer";
 import Navigation from "../../components/Navigation";
 import TrackDetails from "../../components/TrackDetails";
+import { APITrack, deleteTrack, getTrack } from "../../utils/api";
 
 export default function Track() {
   const router = useRouter();
@@ -21,6 +21,11 @@ export default function Track() {
     });
   }, [id]);
 
+  const handleDeleteClick = async () => {
+    await deleteTrack(track.id);
+    router.back();
+  };
+
   if (!track) {
     return <div>Loading...</div>;
   }
@@ -33,11 +38,17 @@ export default function Track() {
 
       <main>
         <TrackDetails
+          id={track.id}
           imgSrc={track.imgSrc}
           title={track.title}
           artist={track.artist}
         />
       </main>
+      <div>
+        <button className={styles.delete} onClick={() => deleteTrack(id)}>
+          DELETE ! TRACK
+        </button>
+      </div>
       <footer>
         <AudioPlayer audio={track.audio} />
       </footer>
